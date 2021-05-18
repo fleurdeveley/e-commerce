56013726cdf03b81e37f18783dde44540cf0cbc6
fleur <fleurdeveley@gmail.com>
2021-05-18 20:31:27 +0200

    home page

--- app/html/src/Controller/TestController.php
+++ app/html/src/Controller/TestController.php
@@ -2,28 +2,18 @@
 
 namespace App\Controller;
 
-use App\Taxes\Calculator;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController 
 {
-    protected $calculator;
-
-    public function __contructor(Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-    }
-
     /**
-     * @Route("/", name="index")
+     * @Route("/test", name="index")
      */
     public function index()
     {
-        $tva = $this->calculator->calcul(100);
-        dump($tva);
-
         dd("Ca fonctionne");
     }
 
